bcf2392418cbedda0eeb3c947b3938e22fc7d472
Aleksey Pardaev <lpardaev607@gmail.com>
2024-11-06 22:23:57 +0300

    Initial commit

---
+++ src/main/java/by/shift/minesweeper/controller/GameController.java
@@ -0,0 +1,30 @@
+package by.shift.minesweeper.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import by.shift.minesweeper.model.Game;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api")
+public class GameController {
+
+    @PostMapping("/game")
+    public Game startNewGame() {
+        return null;
+    }
+
+    @PostMapping("/game/{id}/reveal")
+    public Game revealCell(@PathVariable("id") String id, @RequestParam int row, @RequestParam int col) {
+        return null;
+    }
+
+    @PostMapping("/game/{id}/toggle-flag")
+    public Game toggleFlag(@PathVariable("id") String id, @RequestParam int row, @RequestParam int col) {
+        return null;
+    }
+}
